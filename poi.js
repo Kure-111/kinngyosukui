4d650741ba9cad919ec86da94fdba334273b289a
DESKTOP-RLITA2J\rakuk <rakuk4783@gmail.com>
2023-08-04 13:57:19 +0900

    ポイの描画を改良した、50％で釣れるようにした、ポイが破れるようにした。

--- poi.js
+++ poi.js
@@ -17,13 +17,16 @@ function drawPoi(x, y) {
 
 // マウスを動かしたときのイベントリスナー
 canvas.addEventListener('mousemove', function (e) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // 画面をクリア
-    drawFish(); // 魚を描画
     var rect = canvas.getBoundingClientRect(); // キャンバスの位置情報を取得
     mouseX = e.clientX - rect.left; // マウスのx座標
     mouseY = e.clientY - rect.top; // マウスのy座標
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // 画面をクリア
+
     drawPoi(mouseX, mouseY); // ポイを描画
+    drawFish(); // 魚を描画
 });
+
 
 // キャンバスをクリックしたときのイベントリスナー
 canvas.addEventListener('click', function (e) {
@@ -31,22 +34,26 @@ canvas.addEventListener('click', function (e) {
         var caught = false;
         fishes = fishes.filter(function (fish) {
             if (fish.isCaughtByPoi(mouseX, mouseY)) {
-                score += fish.points;
-                updateScore(score); // スコアを更新
-                caught = true;
+                if (Math.random() <= 0.5) { // 50%の確率で魚が釣れる
+                    score += fish.points;
+                    updateScore(score); // スコアを更新
+                    caught = true;
+                }
                 return false;
             }
             return true;
         });
 
-        if (!caught) { // 魚を捕まえられなかったとき
+        if (!caught) { // 魚を捕まえられなかったとき、または50%の確率で釣りを失敗したとき
             isPoiBroken = true; // ポイを破れた状態にする
             setTimeout(function () {
-                isPoiBroken = false; // 1秒後にポイを元の状態に戻す
-            }, 1000);
+                isPoiBroken = false; // 0.5秒後にポイを元の状態に戻す
+            }, 500);
         }
     }
 });
+
+
 
 
 
